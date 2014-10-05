22c1e7f876bf0da82cb932b266236907915c4953
Tom Stuart <tom@codon.com>
2014-10-05 19:32:12 +0100

    Extract CellularAutomaton class

--- cellular-automaton.js
+++ cellular-automaton.js
@@ -4,29 +4,41 @@
 (function (exports) {
   'use strict';
 
-  var makeCells = function (cellCount) {
-    var cells = [];
-    for (var i = 0; i < cellCount; ++i) {
-      cells[i] = false;
-    }
-    cells[cells.length - 1] = true;
+  var CellularAutomaton = (function () {
+    var makeCells = function (cellCount) {
+      var cells = [];
+      for (var i = 0; i < cellCount; ++i) {
+        cells[i] = false;
+      }
+      cells[cells.length - 1] = true;
 
-    return cells;
-  };
+      return cells;
+    };
 
-  var drawCells = function (cells, context, row, cellSize) {
-    cells.forEach(function (cell, column) {
+    var constructor = function (options) {
+      this.ruleNumber = options.ruleNumber;
+      this.cells = makeCells(options.cellCount);
+    };
+
+    constructor.prototype.rule = function (left, middle, right) {
+      var pattern = (left << 2) | (middle << 1) | (right << 0);
+      return !!(this.ruleNumber & (1 << pattern));
+    };
+
+    constructor.prototype.step = function () {
+      this.cells = this.cells.map(function (middle, column) {
+        return this.rule(this.cells[column - 1], middle, this.cells[column + 1]);
+      }, this);
+    };
+
+    return constructor;
+  }());
+
+  var drawCells = function (cellularAutomaton, context, row, cellSize) {
+    cellularAutomaton.cells.forEach(function (cell, column) {
       if (cell) {
         context.fillRect(column * cellSize, row * cellSize, cellSize, cellSize);
       }
-    });
-  };
-
-  var nextCells = function (cells, ruleNumber) {
-    return cells.map(function (middleCell, column) {
-      var leftCell = cells[column - 1], rightCell = cells[column + 1];
-      var pattern = (leftCell << 2) | (middleCell << 1) | (rightCell << 0);
-      return !!(ruleNumber & (1 << pattern));
     });
   };
 
@@ -47,14 +59,17 @@
     var context = canvas.getContext('2d');
 
     var cellCount = Math.floor(canvas.width / cellSize);
-    var cells = makeCells(cellCount);
+    var cellularAutomaton = new CellularAutomaton({
+      ruleNumber: ruleNumber,
+      cellCount: cellCount
+    });
 
     var rowCount = Math.floor(canvas.height / cellSize);
     var row = 0;
 
     window.setInterval(function () {
-      drawCells(cells, context, row, cellSize);
-      cells = nextCells(cells, ruleNumber);
+      drawCells(cellularAutomaton, context, row, cellSize);
+      cellularAutomaton.step();
 
       if (row < rowCount - 1) {
         ++row;
