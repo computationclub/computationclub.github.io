3db3023538672c80489ed468b59fb9b403f967fa
Tom Stuart <tom@codon.com>
2014-10-08 14:37:16 +0100

    Extract CellGrid into its own file

--- cellular-automaton.js
+++ cellular-automaton.js
@@ -45,50 +45,5 @@
     return constructor;
   }());
 
-  var CellGrid = (function () {
-    var scrollContext = function (context, columns, rows, cellSize) {
-      var width = columns * cellSize, height = rows * cellSize;
-
-      var imageData = context.getImageData(0, cellSize, width, height - cellSize);
-      context.putImageData(imageData, 0, 0);
-      context.clearRect(0, height - cellSize, width, cellSize);
-    };
-
-    var nextRow = function () {
-      if (this.row < this.rows - 1) {
-        ++this.row;
-      } else {
-        scrollContext(this.context, this.columns, this.rows, this.cellSize);
-      }
-    };
-
-    var drawCell = function (context, column, row, size) {
-      context.fillRect(column * size, row * size, size, size);
-    };
-
-    var constructor = function (options) {
-      this.canvas = options.canvas;
-      this.cellSize = options.cellSize;
-
-      this.row = 0;
-      this.context = this.canvas.getContext('2d');
-      this.columns = Math.floor(this.canvas.width / this.cellSize);
-      this.rows = Math.floor(this.canvas.height / this.cellSize);
-    };
-
-    constructor.prototype.draw = function (cellularAutomaton) {
-      for (var column = 0; column < this.columns; ++column) {
-        if (cellularAutomaton.cellAt(column)) {
-          drawCell(this.context, column, this.row, this.cellSize);
-        }
-      }
-
-      nextRow.call(this);
-    };
-
-    return constructor;
-  }());
-
-  exports.CellGrid = CellGrid;
   exports.CellularAutomaton = CellularAutomaton;
 }(this));
